801938a8a8aae46100445e19aec30082d4e48e2b
Jean-Luc Felix <jlfelix522@yahoo.com>
2025-05-30 23:21:42 -0700

    Displayed angle variable

--- mainGame.py
+++ mainGame.py
@@ -1,5 +1,9 @@
 import pygame
 
+pygame.init()
+
+font = pygame.font.Font(None, 36)
+
 def startGame():
     print("Loading Game...")
 
@@ -31,13 +35,13 @@ def startGame():
         key = pygame.key.get_pressed()
 
         if key[pygame.K_a] == True:
-            playerX-=2
+            playerX -= 2
         if key[pygame.K_d] == True:
-            playerX+=2
+            playerX += 2
         if key[pygame.K_w] == True:
-            playerY-=2
+            playerY -= 2
         if key[pygame.K_s] == True:
-            playerY+=2
+            playerY += 2
 
         if key[pygame.K_j] == True:
             angle += 2
@@ -46,9 +50,13 @@ def startGame():
 
         rotated_image = pygame.transform.rotate(playerShip, angle)
         rotated_rec = rotated_image.get_rect(center=(playerX, playerY))
+
+        angle_text = str(angle)
+        angle_surface = font.render(angle_text, True, (255, 255, 255))
     
         screen.blit(background, (0, 0))
         screen.blit(rotated_image, rotated_rec)
+        screen.blit(angle_surface, (100, 50))
         # pygame.display.flip()
 
         # # Update the display
